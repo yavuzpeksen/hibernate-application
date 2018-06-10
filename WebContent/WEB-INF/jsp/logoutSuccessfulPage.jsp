d769537395f5e8b469b9da60208424e0390dedc0
Yavuz <yavuzpeksen1986@hotmail.com>
2018-06-10 04:18:58 +0300

    logout operation and page is added

---
+++ WebContent/WEB-INF/jsp/logoutSuccessfulPage.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+
+<c:set var="contextPath" value="${pageContext.request.contextPath}"/>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	
+	<title>Kod Gemisi</title>
+</head>
+<body>     
+	<div align="center" class="container">
+      <h2>You are logged out</h2>
+      <br>
+      <div style="width: 60%; margin:0 auto;">
+      <a style="height: 40px; width:100px; border:1px solid black; display:block; padding-top:10px;" href="${contextPath}/accesspoint/login"> Login</a> 
+      </div> 
+    </div>      
+</body>
+</html>
--- src/main/java/com/ypeksen/mvc/controller/LoginController.java
+++ src/main/java/com/ypeksen/mvc/controller/LoginController.java
@@ -3,9 +3,12 @@ package com.ypeksen.mvc.controller;
 import java.security.Principal;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +41,14 @@ public class LoginController {
     
 
     return "unauthorized";
+  }
+  
+  @RequestMapping(value="/logout", method = RequestMethod.GET)
+  public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+      Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+      if (auth != null){    
+          new SecurityContextLogoutHandler().logout(request, response, auth);
+      }
+      return "logoutSuccessfulPage";
   }
 }
